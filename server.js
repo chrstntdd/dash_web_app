006581f1c9c3489b5f95ef193c9136f1fa9ad2cb
kiNNectUS <design@kinnectus.com>
2017-05-11 13:16:04 -0400

    Update connection error

--- server.js
+++ server.js
@@ -13,14 +13,13 @@ if(process.env.NODE_ENV == 'production'){
   mongoose.connect('mongodb://heroku_d41xhhbh:8noui905v24of65nfletr5eu5s@ds129469.mlab.com:29469/heroku_d41xhhbh',function(error){
   if (error){
     console.log('error attempting remote connection')
-   
-  }else{
     console.log('connecting to local server')
     mongoose.connect('mongodb://localhost/pogo');
   }
 
 });
 }else{
+      console.log('env is '+ process.env.NODE_ENV + ' connecting locally')
      mongoose.connect('mongodb://localhost/pogo');
 }
 
