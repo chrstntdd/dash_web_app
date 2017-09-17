76a3328791ac1c0908fe7acf02006642f53a4a91
Blake <design@kinnectus.com>
2017-09-17 16:10:26 +0000

    Fix login gradient and form resizing. Add device removal

--- bin/site_maintenance.js
+++ bin/site_maintenance.js
@@ -1,29 +1,30 @@
-// var Site = require('../models/site');
-// var moment = require('moment');
-// var mongoose = require('mongoose');
+var Site = require('../models/site');
+var moment = require('moment');
+var mongoose = require('mongoose');
 
 
 
-//   process.env.NODE_ENV = 'production'
-//   if(process.env.NODE_ENV == 'production'){
-//     mongoose.connect('mongodb://heroku_d41xhhbh:8noui905v24of65nfletr5eu5s@ds129469.mlab.com:29469/heroku_d41xhhbh',function(error){
-//     if (error){
-//       // console.log('error attempting remote connection')
-//       // console.log('connecting to local server')
-//       mongoose.connect('mongodb://localhost/pogo');
-//     }
-//   });
-//   }else{
-//         console.log('env is '+ process.env.NODE_ENV + ' connecting locally')
-//       mongoose.connect('mongodb://localhost/pogo');
-//   }
+  process.env.NODE_ENV = 'production'
+  if(process.env.NODE_ENV == 'production'){
+    mongoose.connect('mongodb://heroku_d41xhhbh:8noui905v24of65nfletr5eu5s@ds129469.mlab.com:29469/heroku_d41xhhbh',function(error){
+    if (error){
+      // console.log('error attempting remote connection')
+      // console.log('connecting to local server')
+      mongoose.connect('mongodb://localhost/pogo');
+    }
+  });
+  }else{
+    console.log('env is '+ process.env.NODE_ENV + ' connecting locally')
+    mongoose.connect('mongodb://localhost/pogo');
+  }
   
-//   Site.getSites(function(err,sites){
-//       if(err) throw err;
-//       if (sites.length > 0){
-//           sites.forEach(function(site){
-//              site.device_ids = [];
-//              site.save()
-//           })
-//       }
-//   })
+  Site.getSites(function(err,sites){
+      if(err) throw err;
+      if (sites.length > 0){
+          sites.forEach(function(site){
+             site.device_ids = [];
+             console.log("updated site devices")
+             site.save()
+          })
+      }
+  })
