d4c3a4ebad8849418ab56ac04e7167e760641a8d
Blake <design@kinnectus.com>
2017-09-25 17:17:33 +0000

    Redirect site model in site_rate.js

--- models/site_rate.js
+++ models/site_rate.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var moment = require('moment');
-var Site = require('site');
+var Site = require('./site');
 var rateSchema = new Schema({
         site: String,
         customer_id: String,
